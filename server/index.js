d2c0b896a164fd8e3309b78ce66d7a52703ebb37
jellyjoji <juicy_joji@yu.ac.kr>
2023-07-03 14:19:59 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+console.log("hellow.js");
+
+const liveServer = require('live-server');
+
+const params = {
+  port:5500,
+  host:'localhost',
+  root:'./client',
+  open:false
+}
+
+liveServer.start(params);
+
